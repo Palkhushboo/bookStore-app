1f3b2693b52f257a101b7b6f16b1c365445463df
khushboo <palkhushboo2632@gmail.com>
2024-05-20 11:42:39 +0530

    first commit

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,14 +3,14 @@ import Navbar from '../Component/Navbar'
 import Banner from '../Component/Banner'
 import Freebook from '../Component/Freebook'
 import Footer from '../Component/Footer'
-import Contact from '../Component/Contact'
+
 function Home() {
   return (
     <>
     <Navbar/>
 <Banner/>
 <Freebook/>
-<Contact/>
+
 
 <Footer/>
 
